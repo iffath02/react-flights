85117f4fdce37efef64f75e3a07d2b5629023356
Caleb Love <mrcaleblove@live.com>
2023-04-21 14:38:53 +1000

    added elonJet button

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,36 +1,47 @@
-import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React, { useState, useRef } from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
 const containerStyle = {
-    width: '400px',
-    height: '400px'
-};
-  
-const center = {
-    lat: -3.745,
-    lng: -38.523
-};
+	width: '90vw',
+	height: '90vh',
+}
 
+const initialCenter = {
+	lat: -3.745,
+	lng: -38.523,
+}
 
+const elonJetCenter = {
+	lat: 33.92074,
+	lng: -118.32704,
+}
 
 function Map() {
+	const [center, setCenter] = useState(initialCenter)
+	const mapRef = useRef(null)
 
-        return (
-          <LoadScript
-            googleMapsApiKey= {process.env.GOOGLE_API_KEY}
-          >
-            <GoogleMap
-              mapContainerStyle={containerStyle}
-              center={center}
-              zoom={10}
-            >
-              { /* Child components, such as markers, info windows, etc. */ }
-              <></>
-            </GoogleMap>
-          </LoadScript>
-        )
+	const handleElonJetClick = event => {
+		event.preventDefault()
+		const map = mapRef.current
+		if (map) {
+			map.panTo(elonJetCenter, 3000)
+		}
+	}
 
+	const handleMapLoad = map => {
+		mapRef.current = map
+	}
 
+	return (
+		<LoadScript googleMapsApiKey={process.env.GOOGLE_API_KEY}>
+			<GoogleMap
+				mapContainerStyle={containerStyle}
+				center={center}
+				zoom={10}
+				onLoad={handleMapLoad}></GoogleMap>
+			<button onClick={handleElonJetClick}>elonJet</button>
+		</LoadScript>
+	)
 }
 
 export default React.memo(Map)
